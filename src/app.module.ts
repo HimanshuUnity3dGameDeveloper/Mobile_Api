2623f60289420a9edcf2ce3fe902bfdb404b0bc8
himanshu <himanshu9084r@gmail.com>
2026-08-30 10:49:51 +0530

    initial commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,7 +9,7 @@ import { FollowModule } from './follow/follow.module';
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017', {
-      dbName: 'MobileApp',
+      dbName: 'socialCircleDB',
     }),
     AuthModule,
     PostModule,
